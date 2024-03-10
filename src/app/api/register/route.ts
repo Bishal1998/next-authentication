d6281595a5907d9d40dfaa2d78a641fbac9e34ea
Bishal1998 <bishalpaudel1998@gmail.com>
2024-03-10 12:38:08 +0545

    user register api

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,64 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectToDb from "@/config/db";
+import User from "@/models/userModel";
+import bcryptjs from "bcryptjs";
+
+const POST = async (req: Request, res: Response) => {
+  try {
+    await connectToDb();
+    const data = await req.json();
+    const { firstName, lastName, email, phoneNumber, password } = data;
+
+    if (!firstName || !lastName || !email || !password || !phoneNumber) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "All Fields are required",
+        },
+        { status: 400 }
+      );
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "User already exists",
+        },
+        { status: 400 }
+      );
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    const user = new User({
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      password: hashedPassword,
+    });
+    await user.save();
+    return NextResponse.json(
+      {
+        success: true,
+        message: "User created successfully",
+        user,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log("Error: ", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Couldn't create user",
+        error,
+      },
+      { status: 500 }
+    );
+  }
+};
+
+export { POST };
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,30 +1,33 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:[true, "First Name is required"]
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "First Name is required"],
     },
-    lastName:{
-        type:String,
-        required:[true, "Last Name is required"]
+    lastName: {
+      type: String,
+      required: [true, "Last Name is required"],
     },
-    email:{
-        type:String,
-        required:[true, "Email is required"],
-        unique:true,
-        trim:true
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
     },
-    phoneNumber:{
-        type:String,
-        required:[true, "Phone is required"],
-        unique:true,
+    phoneNumber: {
+      type: String,
+      required: [true, "Phone is required"],
+      unique: true,
     },
-    password:{
-        type:String,
-        required:[true, "Password is required"]
-    }
-})
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 export default User;
